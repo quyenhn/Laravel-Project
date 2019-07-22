4a1698a0a7d1145531ff8668bf1f023b3886dcc6
quyenhn <kulboy9x96@gmail.com>
2019-07-22 17:12:59 +0700

    fix port nodejs heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var redis = require('redis');
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT);
 console.log("Server dang chay, lang nghe tren cong 3000!");
 io.on('connection',function(socket){
     console.log("Xin chao socketid: "+socket.id+' connected');
