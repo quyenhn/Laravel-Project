3226f0dc70c3678baece41e6de9190fd5f0793be
quyenhn <kulboy9x96@gmail.com>
2019-07-22 11:37:33 +0700

    try_deloy_heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var redis = require('redis');
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 console.log("Server dang chay, lang nghe tren cong 3000!");
 io.on('connection',function(socket){
     console.log("Xin chao socketid: "+socket.id+' connected');
@@ -13,7 +13,7 @@ io.on('connection',function(socket){
     redisClient.on("message",function(channel, message)
     { 
          console.log("Tin nhan moi: "+ message + " on "+ "channel "+channel);
-         socket.emit(channel,message);// server phat tin nhan toi channel
+         io.emit(channel,message);// server phat tin nhan toi channel
     });
 
     socket.on('disconnect',function(){
