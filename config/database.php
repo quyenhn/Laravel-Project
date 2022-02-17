1444616cc71a1ed400bd718e6beb4147b03f8c14
quyenhn <42793878+quyenhn@users.noreply.github.com>
2022-02-17 17:49:51 +0700

    fix redis

--- config/database.php
+++ config/database.php
@@ -139,8 +139,9 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DB', 0),
-            'read_write_timeout' => 0,
         ],
+        
+        'read_write_timeout' => 0,
 
         'cache' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
