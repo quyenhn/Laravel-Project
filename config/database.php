4c4b26bc72115bc28814fe24643dbe7d6fde7c9b
quyenhn <42793878+quyenhn@users.noreply.github.com>
2022-03-20 16:51:05 +0700

    redis scheme

--- config/database.php
+++ config/database.php
@@ -135,6 +135,7 @@ return [
         ],
 
         'default' => [
+            'scheme' => 'tls',
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
