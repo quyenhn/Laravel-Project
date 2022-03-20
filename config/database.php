bb75478f97311302f38f7517db894a8485b291bc
quyenhn <42793878+quyenhn@users.noreply.github.com>
2022-03-20 17:18:16 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -135,15 +135,13 @@ return [
         ],
 
         'default' => [
-            'scheme' => 'tls',
+            //'scheme' => 'tls',
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DB', 0),
-            'read_write_timeout' => -1,
+            'read_write_timeout' => 0,
         ],
-        
-        'read_write_timeout' => -1,
 
         'cache' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
